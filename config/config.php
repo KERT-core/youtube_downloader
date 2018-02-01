77c11b192799b57e2dd07d346599c60010fb2a89
ekstrah <ekstrah2013@knu.ac.kr>
2018-02-01 23:32:10 +0900

    first commit

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+//Default password is "root" with md5 hash
+//No "/" at the end of outputFolder
+
+return array(
+	"security" => true,
+	"password" => "e424b612c066a4adca95dc1b3957efd1",
+	"outputFolder" => "downloads",
+	"extracter" => "avconv",
+	"max_dl" => 3);
+
+?>
